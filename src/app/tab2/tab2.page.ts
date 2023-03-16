fa7a1b1e566cf9b3f61f447c42f405fb4ceb0df7
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-03-16 12:25:54 -0300

    Paranaueee, paranaue, o menino chorou nhe nhe nhe

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,51 @@ export class Tab2Page {
 
   constructor() {}
 
+  value = 1;
+  wePoints = 0;
+  theyPoints = 0;
+  weWins = 0;
+  theyWins = 0;
+
+  changeValue(value:number) {
+    if (this.value < value) {
+      this.value = value;
+    }
+  }
+
+  increaseWePoints() {
+    this.wePoints += this.value;
+    if(this.wePoints === 12) {
+      this.weWins += 1;
+      this.theyPoints = 0;
+      this.wePoints = 0;
+      this.value = 1;
+    }
+  }
+
+  increaseTheyPoints() {
+    this.theyPoints += this.value;
+    if(this.theyPoints >= 12) {
+      this.theyWins += 1;
+      this.theyPoints = 0;
+      this.wePoints = 0;
+      this.value = 1;
+    }
+  }
+
+  decreaseWePoints() {
+    this.wePoints -= this.value;
+  }
+
+  decreaseTheyPoints() {
+    this.theyPoints -= this.value;
+  }
+
+  clearValues() {
+    this.value = 1;
+    this.wePoints = 0;
+    this.theyPoints = 0;
+    this.weWins = 0;
+    this.theyWins = 0;
+  }
 }
